f837eef0362fafde98e5e133e11bbe41891df744
Marzi Ghasedian <marzighased@gmail.com>
2025-07-31 22:17:50 +0200

    cart function added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Book from './book';
 import Cart from './cart';
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([]);
+
   const books = [
     { id: 1, title: "Harry Potter", author: "J.K. Rowling", price: "25" },
     { id: 2, title: "The Little Prince", author: "Antoine de Saint-Exupéry", price: "18" },
@@ -10,10 +13,12 @@ function App() {
     { id: 4, title: "To Kill a Mockingbird", author: "Harper Lee", price: "20" }
   ];
 
+  const totalPrice = cartItems.reduce((sum, item) => sum + item.price, 0);
+
   return (
-    <div>
+    <div className="app">  
       <h1>My Bookstore</h1>
-      <Cart itemCount="0" total="0" />
+      <Cart itemCount={cartItems.length} total={totalPrice} />
       <div className='books-container'>
         {books.map(book => (
           <Book 
@@ -24,7 +29,6 @@ function App() {
           />
         ))}
       </div>
-      
     </div>
   );
 }
